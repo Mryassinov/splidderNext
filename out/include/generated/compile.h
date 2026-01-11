5d4ab7b169e4041cfa0b15ea48f563b9309a15ab
Mryassinov <yassine.98.test4@gmail.com>
2026-01-11 23:16:27 +0100

    Add KernelSU Next v3.0.0 support
    - Integrated KernelSU Next v3.0.0 (build 32940)
    - Hook mode: Kprobes (legacy)
    - Applied kernel patches for namespace, seccomp, SELinux
    - Manager signature: 79e590113c4c4c0c222978e413a5faa801666957b1212a328e46c00c69821bf7
    - Successfully built Splidder-KSUN kernel
    - Build time: 4m 53s
    
    Kernel features:
    - Full KernelSU Next root management
    - Module support
    - SELinux patches applied
    - Compatible with KernelSU Next Manager only

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#1 SMP PREEMPT Sun Jan 11 22:50:12 +01 2026"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Jan 11 23:13:51 +01 2026"
 #define LINUX_COMPILE_BY "yassine"
 #define LINUX_COMPILE_HOST "splidder-build"
 #define LINUX_COMPILER "Android (13174946, +pgo, +bolt, +lto, +mlgo, based on r547379) clang version 20.0.0, LLD 20.0.0 (/mnt/disks/build-disk/src/android/llvm-r547379-release/out/llvm-project/llvm b718bcaf8c198c82f3021447d943401e3ab5bd54)"
